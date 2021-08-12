5cc5b6372b77c53b3c9af4116a95eccd4dc2c336
裏住 隆幸 <i.10.me.tu@gmail.com>
2021-08-12 17:44:55 +0900

    1st commit

---
+++ lesson.py
@@ -0,0 +1,18 @@
+import math
+
+result = math.sqrt(25)
+print(result)
+
+y = math.log2(10)
+print(y)
+
+a = 'a'
+print(f'a is {a}')
+
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
+print(f'a is {z}, {y}, {x}')
+
+name = 'Jun'
+family = 'Sakai'
+print(f'My name is {name} {family}. Watashi ha {family} {name}')
